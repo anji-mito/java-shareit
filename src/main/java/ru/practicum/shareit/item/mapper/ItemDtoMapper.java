68e4212804979d7d36164e4eb5ceb08c13aa2d07
anji-mito <120057673+anji-mito@users.noreply.github.com>
2023-07-11 14:56:23 -0700

    add: ItemDtoMapper

--- src/main/java/ru/practicum/shareit/item/mapper/ItemDtoMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemDtoMapper.java
@@ -1,10 +1,15 @@
 package ru.practicum.shareit.item.mapper;
 
+import org.springframework.stereotype.Service;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
-public class ItemMapper {
-    public static ItemDto toItemDto(Item item) {
+import java.util.function.Function;
+
+@Service
+public class ItemDtoMapper implements Function<Item, ItemDto> {
+    @Override
+    public ItemDto apply(Item item) {
         return new ItemDto(
                 item.getName(),
                 item.getDescription(),
