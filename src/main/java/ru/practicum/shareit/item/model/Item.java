0d963aa27b531aac67d55debe3cd9d9ea9ce5f0b
ckstr <ckstr@mail.ru>
2023-07-27 18:02:02 +0500

    add-bookings

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -7,15 +7,24 @@ import lombok.NoArgsConstructor;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @AllArgsConstructor
 @Builder
 @Data
 @NoArgsConstructor
+@Entity
+@Table(name = "items")
 public class Item {
+    @Id
     private long id;
     private String name;
     private String description;
     private Boolean available;
+    @Column(name = "owner_id", nullable=false)
     private User owner;
     private ItemRequest request;
 }
