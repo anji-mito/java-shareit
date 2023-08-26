cbaf5f4707e710f74ccbe9ef32b28fdf6fef79f5
anji-mito <120057673+anji-mito@users.noreply.github.com>
2023-08-26 13:01:12 -0700

    fix: style

--- server/src/test/java/TestConfig.java
+++ server/src/test/java/TestConfig.java
@@ -1,10 +1,10 @@
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
-@Configuration
-@Profile("test")
+@SpringBootTest
+@ActiveProfiles("ci,test")
 public class TestConfig {
     @Bean
     public ModelMapper testModelMapper() {
